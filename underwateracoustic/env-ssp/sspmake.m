3a7221d0d0a9f7bed743c25ae30df3d67b4379e9
AmazingUmi7 <AmazingUmi7@gmail.com>
2024-05-07 20:36:09 +0800

    test

--- underwateracoustic/env-ssp/sspmake.m
+++ underwateracoustic/env-ssp/sspmake.m
@@ -1,3 +1,5 @@
-clc
-clear all
-close all
+clear; close all; clc;
+tmp = matlab.desktop.editor.getActive;
+index = strfind(tmp.Filename, '\') ;
+pathstr = tmp.Filename(1:index(end)-1);
+cd(pathstr);
